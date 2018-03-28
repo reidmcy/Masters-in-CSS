ad26503a1cee0c8f1e0c3ee991c01a36696a854a
Reid McIlroy-Young <reid@reidmcy.com>
2018-03-28 02:47:46 -0500

    Genenerting working W2V

---
+++ gen_w2v.py
@@ -0,0 +1,14 @@
+import pandas
+import gensim
+import io
+import simple_id
+import simple_id.utilities
+
+target = '../data/full_ss.tsv'
+
+def main():
+    df_data = pandas.read_csv(target, sep = '\t')
+    simple_id.preprocesing(df_data, 'w2v', 'w2v.bin', 'full_pickle.p', 200, useFeather = False)
+
+if __name__ == '__main__':
+    main()
