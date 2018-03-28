ee1fe1b441cdf0714f6b9cdb5b334b52de83ad33
Reid McIlroy-Young <reid@reidmcy.com>
2018-03-28 02:53:23 -0500

    Small fix

--- gen_w2v.py
+++ gen_w2v.py
@@ -7,7 +7,7 @@ import simple_id.utilities
 target = '../data/full_ss.tsv'
 
 def main():
-    df_data = pandas.read_csv(target, sep = '\t')
+    df_data = pandas.read_csv(target, sep = '\t', error_bad_lines=False)
     simple_id.preprocesing(df_data, 'w2v', 'w2v.bin', 'full_pickle.p', 200, useFeather = False)
 
 if __name__ == '__main__':
